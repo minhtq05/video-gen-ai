78968906262a9f8f6ee6240614438d570a56017c
minhtq05 <minhtq2005@gmail.com>
2025-04-04 13:29:31 -0400

    new source

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,49 @@
 import type { Metadata } from "next";
-import {
-	ClerkProvider,
-	SignInButton,
-	SignUpButton,
-	SignedIn,
-	SignedOut,
-	UserButton,
-} from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import { ClerkLoaded, ClerkLoading, ClerkProvider } from "@clerk/nextjs";
+import { Providers } from "./providers";
+import { Suspense } from "react";
+import Loading from "@/components/loading";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "S2L",
-	description: "Slide 2 Lecture Video Editor",
+  title: "Video Gen AI",
+  description: "Video Gen AI",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<ClerkProvider>
-			<html lang="en">
-				<body
-					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-				>
-					<header className="flex justify-end items-center p-1 gap-4 h-16">
-						<SignedOut>
-							<SignInButton />
-							<SignUpButton />
-						</SignedOut>
-						<SignedIn>
-							<UserButton />
-						</SignedIn>
-					</header>
-					{children}
-				</body>
-			</html>
-		</ClerkProvider>
-	);
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen`}
+        >
+          <Suspense fallback={<div>Loading...</div>}>
+            <Providers>
+              <ClerkLoading>
+                <Loading />
+              </ClerkLoading>
+              <ClerkLoaded>{children}</ClerkLoaded>
+            </Providers>
+          </Suspense>
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
